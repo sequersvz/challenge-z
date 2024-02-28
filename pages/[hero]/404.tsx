7a7f18b1ba98eb79c61b3e2f1f4bf9f292350b8d
Luis Estraño <luisestrano24@gmail.com>
2024-02-28 12:08:01 +0100

    Initial Structure

---
+++ pages/[hero]/404.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/pages/index.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Hero</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>FFF</h1>
+      </main>
+
+    </div>
+  );
+}
